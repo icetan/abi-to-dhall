4768dfe287ece33785395f1147c07f27f2da39fb
Christopher Fredén <me@icetan.org>
2020-01-30 13:28:14 +0100

    Fix dhall bin URL for macOS

--- default.nix
+++ default.nix
@@ -6,18 +6,20 @@
     inherit name src;
     installPhase = ''
       mkdir -p $out/bin
-      cp ./bin/* $out/bin
+      [ ! -d ./bin ] || cd ./bin
+      cp -t $out/bin ./*
     '';
   } // args;
 
   dhallHaskellPackage = { version, subName ? null, subVersion, sha256 }: let
+    system = if pkgs.stdenv.isDarwin then "x86_64-macos" else builtins.currentSystem;
     namePart = optionalString (subName != null) "${subName}-";
     name = "dhall-${namePart}bin-${subVersion}";
   in binPackage {
     inherit name;
     src = fetchTarball {
-      inherit sha256;
-      url = "https://github.com/dhall-lang/dhall-haskell/releases/download/${version}/dhall-${namePart}${subVersion}-${builtins.currentSystem}.tar.bz2";
+      sha256 = sha256."${system}";
+      url = "https://github.com/dhall-lang/dhall-haskell/releases/download/${version}/dhall-${namePart}${subVersion}-${system}.tar.bz2";
     };
   };
 
@@ -27,8 +29,20 @@
     name = "dhall-haskell-${version}";
     ignoreCollisions = true;
     paths = map (x: dhallHaskellPackage ({ inherit version; } // x)) [
-      { subName = null   ; subVersion =  version ; sha256 = "0nfpppsg2ahdrjkpczifcn5ixc55lc3awxrbggkcd72gf0539abr"; }
-      { subName = "json" ; subVersion = "1.6.1"  ; sha256 = "0qddrngfl926dgl29x2xdh32cgx94wkcnsm94kyx87wb4y3cbxga"; }
+      { subName = null;
+        subVersion = version;
+        sha256 = {
+          x86_64-linux = "0nfpppsg2ahdrjkpczifcn5ixc55lc3awxrbggkcd72gf0539abr";
+          x86_64-macos = "0impdrphm7wsm5i17dwyq12i1nrirq78n20kbjz68ahyrf1b8ld7";
+        };
+      }
+      { subName = "json";
+        subVersion = "1.6.1";
+        sha256 = {
+          x86_64-linux = "0qddrngfl926dgl29x2xdh32cgx94wkcnsm94kyx87wb4y3cbxga";
+          x86_64-macos = "061yxgxh376qf5mha5glzayhjs6qgw3vv6acxzh3zaiz424p1bvz";
+        };
+      }
     ];
   };
 
